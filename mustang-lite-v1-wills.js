ee276f42cb55b9d42b646c87c2e02bcf1ae36a43
icewhisp <67015494+icewhisp@users.noreply.github.com>
2020-11-22 13:57:54 -0600

    update
    Let's go

--- mustang-lite-v1-wills.js
+++ mustang-lite-v1-wills.js
@@ -1,10 +1,13 @@
 var contactURLArray = [];
 var contactArray = [];
+var currentContact;
 var posInArray = 0;
+
 
 
 function printIndex() {
     var indexRequest = new XMLHttpRequest();
+    alert("Hello and welcome to my Mustang Version 2, if you enter zip codes first it will autofill the rest of the information!");
     indexRequest.open('GET', 'https://mustang-index.azurewebsites.net/index.json');
     indexRequest.onload = function() {
         console.log("Index JSON:" + indexRequest.responseText);
@@ -27,7 +30,9 @@ function printIndex() {
 function loadContacts() {
     contactArray.length = 0;
     posInArray = 0;
+    
     if (contactURLArray.length > posInArray) { //I tried to make this a while loop here but it would just print out he first contact over and over
+        console.log("loading a contact");
         loadNextContact(contactURLArray[posInArray]);
         
     }
@@ -42,18 +47,19 @@ function loadNextContact(URL) {
         console.log(contactRequest.responseText);
         var contact;
         contact = JSON.parse(contactRequest.responseText);
-        console.log("Contact: " + contact.firstName + " " + contact.lastName);
+        console.log("Contact: " + contact.firstName);
         contactArray.push(contact);
-        contactText  = JSON.stringify(contactArray);
-        contactText = contactText.replace(/}/g, "} <br/><br/>");
-        contactText = contactText.replace(/]/g," ");
-        contactText = contactText.replace(/\[/g," ");
-        contactText = contactText.replace(/,/g," ");
-        contactText = contactText.replace(/"/g," ");
-        document.getElementById("contactsID").innerHTML = contactText;
+
+        document.getElementById("contactsID").innerHTML = JSON.stringify(contactArray);
+
         posInArray++;
         if (contactURLArray.length > posInArray) {
             loadNextContact(contactURLArray[posInArray]);
+        }
+        else {
+            posInArray = 0;
+            displayContact()
+            console.log(contactArray);
         }
     }
 
@@ -64,6 +70,84 @@ function logContacts() {
     console.log(contactArray);
 }
 
+function nextConctact(){
+    if (posInArray < (contactArray.length-1)) {
+        posInArray++;
+    }
+    currentContact = contactArray[posInArray];
+    displayContact();
 
+}
+function previous(){
+    if (posInArray > 0) {
+        posInArray--;
+    }
+    currentContact = contactArray[posInArray];
+    displayContact();
 
+}
+function displayContact(){
+    console.log(posInArray);
+    currentContact = contactArray[posInArray];
+    console.log(currentContact);
+    
+    document.getElementById("nameID").value = currentContact.preferredName;
+    document.getElementById("emailID").value = currentContact.email;   
+    document.getElementById("cityID").value = currentContact.city;   
+    document.getElementById("stateID").value = currentContact.state;
+    document.getElementById("zipID").value = currentContact.zip;  
 
+}
+function del()
+{
+    console.log("Deleting: " + contactArray[posInArray]);
+    contactArray.splice(posInArray,1);
+    displayContact()
+
+}
+function add(){
+    var newContact = {"preferredName": document.getElementById("nameID").value, "email":document.getElementById("emailID").value,"city":document.getElementById("cityID").value,"state":document.getElementById("stateID").value,"zip":document.getElementById("zipID").value};
+    contactArray.splice(posInArray,0,newContact);
+    document.getElementById("nameID").value = "Added";
+    document.getElementById("emailID").value = "Added";   
+    document.getElementById("cityID").value = "Added";   
+    document.getElementById("stateID").value = "Added";
+    document.getElementById("zipID").value = "Added";  
+    
+}
+function updated(){
+    var newContact = {"preferredName": document.getElementById("nameID").value, "email":document.getElementById("emailID").value,"city":document.getElementById("cityID").value,"state":document.getElementById("stateID").value,"zip":document.getElementById("zipID").value};
+    contactArray.splice(posInArray,1,newContact);
+    document.getElementById("nameID").value = "Updated";
+    document.getElementById("emailID").value = "Updated";   
+    document.getElementById("cityID").value = "Updated";   
+    document.getElementById("stateID").value = "Updated";
+    document.getElementById("zipID").value = "Updated";  
+    
+}
+
+function getLoc()
+{
+    var zip =  document.getElementById("zipID").value;
+
+}
+
+function getPlace() {
+    var zip = document.getElementById("zipID").value
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "CityState.php?zip=" + zip);
+    // Register the embedded handler function
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            var result = xhr.responseText;
+            console.log("result:"+result);
+            var place = result.split(', ');
+            if (document.getElementById("cityID").value == "")
+                document.getElementById("cityID").value = place[0];
+            if (document.getElementById("stateID").value == "")
+                document.getElementById("stateID").value = place[1];
+        }
+    }
+    
+    xhr.send(null);
+}
